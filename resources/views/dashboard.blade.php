80764cadc0f6f96f6eb46ef4671de5d6c2bfb5a3
cc201007 <cc201007@fhstp.ac.at>
2023-01-30 18:44:43 +0100

    creating tables for vacation requests

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -14,5 +14,56 @@
             </div>
         </div>
     </div>
+
+    <div class="pb-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <h2 class="p-6 text-gray-900">
+                    {{ __("Urlaubsanträge: ") }}
+                </h2>
+
+                <div class="pb-4">
+                    <div class="w-2/3 mx-auto">
+                        <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+                            <table class="w-full text-sm text-left text-gray-500">
+                                <thead class="text-xs text-gray-700 uppercase bg-gray-200">
+                                <tr>
+                                    <th scope="col" class="px-6 py-3">
+                                        Start Datum
+                                    </th>
+                                    <th scope="col" class="px-6 py-3">
+                                        End Datum
+                                    </th>
+                                    <th scope="col" class="px-6 py-3">
+                                        Gesamte Urlaubstage
+                                    </th>
+                                    <th scope="col" class="px-6 py-3">
+                                        <span class="sr-only">Edit</span>
+                                    </th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                <tr class="bg-white border-b">
+                                    <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
+                                        Apple MacBook Pro 17"
+                                    </th>
+                                    <td class="px-6 py-4">
+                                        Sliver
+                                    </td>
+                                    <td class="px-6 py-4">
+                                        Laptop
+                                    </td>
+                                    <td class="px-6 py-4 text-right">
+                                        <a href="#" class="font-medium text-blue-600 hover:underline">Edit</a>
+                                    </td>
+                                </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </x-app-layout>
 
